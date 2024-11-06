88d490e00c8cd127ae1cc7c1b6433e8da70517ea
Ivan Luk <luk.ivan.mech@gmail.com>
2024-11-06 02:03:40 -0800

    Setup useSwr to expose data, error, and loading states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Advocates } from "./types/advocates";
+import useSWR from "swr";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
-
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
+  const [advocates, setAdvocates] = useState<Advocates[]>([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocates[]>([]);
+  const fetcher = (url: string) =>
+    fetch(url).then((response) => {
       response.json().then((jsonResponse) => {
         setAdvocates(jsonResponse.data);
         setFilteredAdvocates(jsonResponse.data);
       });
     });
-  }, []);
+  const { data, error, isLoading } = useSWR("/api/advocates", fetcher);
 
   const onChange = (e) => {
     const searchTerm = e.target.value;
@@ -28,8 +28,7 @@ export default function Home() {
         advocate.lastName.includes(searchTerm) ||
         advocate.city.includes(searchTerm) ||
         advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
+        advocate.specialties.includes(searchTerm)
       );
     });
 
---
+++ src/app/types/advocates.ts
@@ -0,0 +1,11 @@
+export interface Advocates {
+  city: string;
+  createdAt: string;
+  degree: string;
+  firstName: string;
+  id: number;
+  lastName: string;
+  phoneNumber: number;
+  specialties: string[];
+  yearsOfExperience: number;
+}
